1a5a52c2ebf2e02cbd40db4458719d2b32b94e7f
ElminDmitry <elmin.mailbox@gmail.com>
2017-09-12 16:27:41 +0300

    create topics

---
+++ db/migrate/20170912132656_create_topics.rb
@@ -0,0 +1,23 @@
+class CreateTopics < ActiveRecord::Migration[5.1]
+  def change
+    create_table :topics do |t|
+      t.string :title
+      t.text :content
+
+      t.integer :user_id
+      t.index :user_id
+
+      t.integer :group_id
+      t.index :group_id
+
+      t.integer :theme_id
+      t.index :theme_id
+
+      t.json :last_post
+      t.integer :posts_count, default: 0
+
+
+      t.timestamps
+    end
+  end
+end
