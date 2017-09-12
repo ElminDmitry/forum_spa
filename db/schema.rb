4a7cbf88ffea43855b8f9c7f47d2c710c90ae86b
ElminDmitry <elmin.mailbox@gmail.com>
2017-09-12 16:31:14 +0300

    create users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912132749) do
+ActiveRecord::Schema.define(version: 20170912132856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,32 @@ ActiveRecord::Schema.define(version: 20170912132749) do
     t.index ["group_id"], name: "index_topics_on_group_id"
     t.index ["theme_id"], name: "index_topics_on_theme_id"
     t.index ["user_id"], name: "index_topics_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "name"
+    t.boolean "banned", default: false
+    t.integer "avatar_id"
+    t.string "avatar_url", default: "/default_avatar.png"
+    t.integer "rating", default: 0
+    t.integer "votes", default: [], array: true
+    t.integer "posts_count", default: 0
+    t.integer "topics_count", default: 0
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
