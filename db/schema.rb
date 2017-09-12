39e74e4a2e396e12fb18306f01b6535a09dc392e
ElminDmitry <elmin.mailbox@gmail.com>
2017-09-12 16:32:08 +0300

    create avatars

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912132856) do
+ActiveRecord::Schema.define(version: 20170912133124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "avatars", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "title"
