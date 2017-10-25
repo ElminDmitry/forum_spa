781a566a4b095dc6f14c2e513d939947c3b26d9d
Unknown <elmin.mailbox@gmail.com>
2017-10-25 17:16:00 +0300

    Add authorization & authentication(with refactoring)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!, only: [:create, :update, :destroy, :new, :edit]
+
+
+  private
+
+  def authenticate_user!
+    unless current_user
+      if request.xhr?
+        render json: { msg: 'Вы не авторизованы' }, status: 403
+      else
+        redirect_to root_path
+      end
+    end
+  end
+
+  protected
+
+  def verify_captcha response
+    result = RestClient.post(
+        'https://www.google.com/recaptcha/api/siteverify',
+        secret: Rails.application.secrets[:recaptcha]['secret_key'],
+        response: response)
+
+    JSON.parse(result)['success']
+  end
 end
---
+++ app/controllers/auth/sessions_controller.rb
@@ -0,0 +1,33 @@
+class Auth::SessionsController < Devise::SessionsController
+  skip_before_action :authenticate_user!
+  after_filter :set_csrf_headers, only: [:create, :destroy]
+
+  def create
+    # render json: { msg: 'Проверка каптчи не пройдена' }, status: 422 and return unless verify_captcha(params[:user][:recaptcha])
+    # self.resource = warden.authenticate(auth_options)
+    # render json: { msg: 'Email не найден, либо пароль неверен' }, status: 401 and return unless self.resource
+    # sign_in(resource_name, self.resource)
+    # render json: { msg: 'Вы успешно авторизовались в системе', current_user: current_user.public_fields }
+    if verify_captcha(params[:user][:recaptcha])
+      self.resource = warden.authenticate(auth_options)
+      if self.resource
+        sign_in(resource_name, self.resource)
+        render json: { msg: 'Вы успешно авторизовались в системе', current_user: current_user.public_fields }
+      else
+        render json: { msg: 'Email не найден, либо пароль неверен' }, status: 401
+      end
+    else
+      render json: { msg: 'Проверка каптчи не пройдена' }, status: 422
+    end
+  end
+
+  def destroy
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    render json: { msg: 'Вы успешно вышли' }
+  end
+
+  protected
+  def set_csrf_headers
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+end
